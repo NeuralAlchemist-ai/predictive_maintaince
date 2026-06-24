69c48aaa3dde8ef7e30ffe804f346acb66e66a47
Tsimur Bardziyan <timurbardiyan@gmail.com>
2026-06-24 22:45:09 +0200

    refactor(config): migrate pipeline parameters to centralized dataclass

--- config.py
+++ config.py
@@ -1,2 +1,15 @@
-DATA_PATH='./datasets/'
-URL='http://localhost:8080'
+from dataclasses import dataclass
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from typing import Type
+
+@dataclass
+class PipelineConfig:
+    data_path: str = "./data"
+    dataset_name: str = "FD002"
+    random_state: int = 42
+    num_clusters: int = 6
+    url: str = 'http://localhost:8080'
+
+    cluster_model_class: Type = KMeans
+    scaler_class: Type = StandardScaler
